20b6b44416a3b4cdbdbf931eb55a60ec9d7cc14c
Michael Morscher <morscher@hm.edu>
2016-03-03 22:41:47 +0000

    Small backend improvements

--- backend.py
+++ backend.py
@@ -1,6 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 from database import Database
 import json
+import datetime
+
+__author__ = 'Michael Morscher'
+__description__ = 'REST backend service'
+__copyright__ = "Copyright 2016"
+__version__ = "0.1"
+__maintainer__ = "Michael Morscher"
+__email__ = "morscher@hm.edu"
+__status__ = "in development"
 
 # database configuration
 database_host = "127.0.0.1"
@@ -10,6 +22,7 @@ database_name = "steam"
 database = Database(database_host, database_user, database_password, database_name)
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -24,17 +37,21 @@ def block():
 @app.route("/block/day/<day>/user/<user>")
 def block_day(day, user):
     value = database.block_playtime_day(user, day)[0][0]
-    data = [{'value': int(value)}]
-    data_string = json.dumps(data)
-    return data_string
+    if value is not None:
+        data = [{'value': int(value)}]
+    else:
+        data = [{'value': 0}]
+    return json.dumps(data)
 
 
 @app.route("/block/month/total/<month>/user/<user>")
 def block_month_total(month, user):
     value = database.block_playtime_month(user, month)[0][0]
-    data = [{'value': int(value)}]
-    data_string = json.dumps(data)
-    return data_string
+    if value is not None:
+        data = [{'value': int(value)}]
+    else:
+        data = [{'value': 0}]
+    return json.dumps(data)
 
 
 @app.route("/block/month/details/<month>/user/<user>/")
@@ -43,7 +60,7 @@ def block_month_details(month, user):
 
     # check for every day
     for day in range(01, 32):
-        
+
         # get data
         date = "{0}-{1:0=2d}".format(month, day)
         result = database.block_playtime_day(user, date)[0][0]
@@ -57,6 +74,5 @@ def block_month_details(month, user):
     return json.dumps(value)
 
 
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
