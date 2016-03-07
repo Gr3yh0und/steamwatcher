1e36fa9c15efe51e0806a3732cf2aaa659436097
Michael Morscher <morscher@hm.edu>
2016-03-07 20:02:40 +0000

    Charts are now loaded asynchronous

--- webfrontend/js/diagrams.js
+++ webfrontend/js/diagrams.js
@@ -4,17 +4,18 @@ google.charts.load('current', {
 google.charts.setOnLoadCallback(drawMonthlyDetails);
 google.charts.setOnLoadCallback(drawAppDistribution);
 google.charts.setOnLoadCallback(drawYearlyPlaytime);
+google.charts.setOnLoadCallback(drawWeeklyDetails);
 google.charts.setOnLoadCallback(drawCalendar);
 
-function drawMonthlyDetails() {
 
-    var jsonData = $.ajax({
+function drawMonthlyDetails(url) {
+    $.ajax({
         url: "http://127.0.0.1:5000/block/month/details/2016-02/user/4/",
         dataType: "json",
-        async: false
-    }).responseText;
-
-    var options = {
+        async: true
+    }).done(function(result){
+		
+		var options = {
         title: 'Playtime for 2016-02',
         isStacked: true,
         width: 1200,
@@ -27,95 +28,99 @@ function drawMonthlyDetails() {
         }
     };
 
-    var data = new google.visualization.DataTable(jsonData);
+    var data = new google.visualization.DataTable(result);
     var chart = new google.visualization.ColumnChart(document.getElementById('chart_monthly_details'));
     chart.draw(data, options);
+	});   
 }
 
 function drawAppDistribution() {
-    var jsonData = $.ajax({
+    $.ajax({
         url: "http://127.0.0.1:5000/block/month/playtime/2016-02/user/4/",
         dataType: "json",
-        async: false
-    }).responseText;
+        async: true
+    }).done(function(result){
 
-    var options = {
-        title: 'Games',
-        pieHole: 0.4,
-        height: 450,
-    };
+		var options = {
+			title: 'Games in month 2016-02',
+			//pieHole: 0.4,
+			height: 450,
+		};
 
-    var data = new google.visualization.DataTable(jsonData);
-    var chart = new google.visualization.PieChart(document.getElementById('chart_app_distribution'));
-    chart.draw(data, options);
+		var data = new google.visualization.DataTable(result);
+		var chart = new google.visualization.PieChart(document.getElementById('chart_app_distribution'));
+		chart.draw(data, options);
+		
+	});
 }
 
 function drawYearlyPlaytime() {
-
-    var jsonData = $.ajax({
+    $.ajax({
         url: "http://127.0.0.1:5000/block/month/last12/user/4/",
         dataType: "json",
-        async: false
-    }).responseText;
+        async: true
+    }).done(function(result){
 
-    var options = {
-        title: 'Playtime per Month in the year 2016',
-        hAxis: {
-            title: 'Month'
-        },
-        vAxis: {
-            title: 'Playtime in Minutes'
-        }
-    };
+		var options = {
+			title: 'Playtime per Month in the year 2016',
+			hAxis: {
+				title: 'Month'
+			},
+			vAxis: {
+				title: 'Playtime in Minutes'
+			},
+			legend: 'none'
+		};
 
-    var data = new google.visualization.DataTable(jsonData);
-    var chart = new google.visualization.ColumnChart(
-        document.getElementById('chart_yearly_playtime'));
+		var data = new google.visualization.DataTable(result);
+		var chart = new google.visualization.ColumnChart(
+			document.getElementById('chart_yearly_playtime'));
 
-    chart.draw(data, options);
+		chart.draw(data, options);
+	});
 }
 
-
 function drawCalendar() {
-    var jsonData = $.ajax({
+    $.ajax({
         url: "http://127.0.0.1:5000/block/month/last365/user/4/",
         dataType: "json",
-        async: false
-    }).responseText;
+        async: true
+    }).done(function(result){
 
-    var data = new google.visualization.DataTable(jsonData);
-    var chart = new google.visualization.Calendar(document.getElementById('calendar_basic'));
+		var data = new google.visualization.DataTable(result);
+		var chart = new google.visualization.Calendar(document.getElementById('calendar_basic'));
 
-    var options = {
-        title: "Playtime calendar",
-        height: 350,
-    };
+		var options = {
+			title: "Playtime calendar",
+			height: 350,
+		};
 
-    chart.draw(data, options);
+		chart.draw(data, options);
+	});
 }
 
 function drawWeeklyDetails() {
-
-    var jsonData = $.ajax({
-        url: "http://127.0.0.1:5000/block/week/last/user/4/",
+	$.ajax({
+        url: "http://127.0.0.1:5000/block/last/days/7/user/4/",
         dataType: "json",
-        async: false
-    }).responseText;
+        async: true
+    }).done(function(result){
 
-    var options = {
-        title: 'Playtime for last 7 days',
-        isStacked: true,
-        width: 1200,
-        height: 450,
-        hAxis: {
-            title: 'Day'
-        },
-        vAxis: {
-            title: 'Playtime in Minutes'
-        }
-    };
+		var options = {
+			title: 'Playtime for last 7 days',
+			isStacked: true,
+			width: 1200,
+			height: 450,
+			hAxis: {
+				title: 'Day'
+			},
+			vAxis: {
+				title: 'Playtime in Minutes'
+			}
+		};
 
-    var data = new google.visualization.DataTable(jsonData);
-    var chart = new google.visualization.ColumnChart(document.getElementById('chart_weekly_playtime'));
-    chart.draw(data, options);
+		var data = new google.visualization.DataTable(result);
+		var chart = new google.visualization.ColumnChart(document.getElementById('chart_weekly_playtime'));
+		chart.draw(data, options);
+	});
 }
